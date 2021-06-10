bac1d20e936428ef5ba9b237a466e349846a9077
Collins Nwoko <collinsnwoko05@gmail.com>
2021-06-10 15:42:55 +0100

    rename sub-category sql and added items on it, category model

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -1,18 +1,29 @@
 <?php
 
 class Api extends Controller{
+    private $msg;
+    public function __construct() {
+        // $header = apache_request_headers(); 
+        // if(!isset($header['gnice-Authenticate'])){
+
+        //}
+    }
     public function signUpApi(){
         //$this->
         $signup = $this->model('Authenticated')->signup();
     }
+    public function create_category(){
 
+    }
     public function fetch_all_category(){
-        $header = apache_request_headers(); 
+       $header = apache_request_headers(); 
         if(isset($header['gnice-Authenticate'])){
             $result = $this->model('Category')->getAllCategory();
             print_r(json_encode($result));
-        }else{
-            echo "invalid request";
+            var_dump($result);
+
+       }else{
+            print_r('not authorised');
             exit;	
         }
     }
