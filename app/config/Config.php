1231dcbdf08ed289d8dbb3a32bbe5443c33778bf
michael <mike98989@gmail.com>
2021-06-10 13:50:19 +0100

    gitignore not working

--- app/config/Config.php
+++ app/config/Config.php
@@ -5,7 +5,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'root');
-    define('DB_NAME', 'gniceddd');
+    define('DB_NAME', 'gnice');
     //app Root
     define('APP_ROOT',dirname(dirname(__FILE__)));
     
