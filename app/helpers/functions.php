c9f1b6ccd65ef60f0efedbbd2bca6c66daf72a78
Collins Nwoko <collinsnwoko05@gmail.com>
2021-06-17 09:43:30 +0100

    added generateToken() in helpers/function

--- app/helpers/functions.php
+++ app/helpers/functions.php
@@ -28,6 +28,22 @@ function generateCode()
 }
 
 
+function generateToken($length){
+	$array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+	$code = '';
+
+	$length = rand(30, $length);
+
+	for ($i=0; $i < $length; $i++) { 
+
+		$random = rand(0, 100);
+
+		$code .= $array[$random];
+	}
+
+	return $code;
+}
+
 
 // form sanitizations
 function sanitizeData($data){
