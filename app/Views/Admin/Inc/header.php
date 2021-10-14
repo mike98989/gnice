a4a60e4d6211938e4cc44f136ebc3a74e82617dc
Collins Nwoko C <collinsnwoko05@gmail.com>
2021-10-14 19:06:40 +0100

    added category preview upload

--- app/Views/Admin/Inc/header.php
+++ app/Views/Admin/Inc/header.php
@@ -26,7 +26,7 @@
                             <span class="availability-status online"></span>
                         </div>
                         <div class="nav-profile-text">
-                            <p class="mb-1 text-black">{{user_data.fullname}}</p>
+                            <p class="mb-1 text-black"> Welcome ,{{user_data.fullname}}</p>
                         </div>
                     </a>
                     <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
--- public/assets/js/controllers/admindashboard/categoryController.js
+++ public/assets/js/controllers/admindashboard/categoryController.js
@@ -37,7 +37,85 @@ module.controller("categoryController", [
     } else {
       pager = "1";
     }
+    $scope.show_image = function (event) {
 
+      const Irray =[];
+      const preview = document.querySelector('.preview');
+      const images = event.target.files;
+
+      $(".number_of_upload").html(`${images.length} images selected`);
+
+      /*
+      for( i = 0; i < images.length; i++){
+          let img = images[i]
+          let reader = new FileReader();
+          reader.onload = (function(event){
+          console.log(img.result);
+          let getTarget = event.target;
+          $(`
+            
+          <div class="position-relative fig  flex-grow-1" style="width: 100px;min-height: 100px; height:100px;">
+          <img src="${event.target.result}" class="block" alt="" style="width: 100px; height:100px;">
+          <a href="" class="stretched-link remove-img">
+            <i class="mdi mdi-close text-dark"
+              style="position: absolute; left: 50%;top: 50%; transform: translate(-50%,-50%) ;cursor: pointer;"></i>
+          </a>
+        </div>
+          
+            `).insertAfter(".preview");
+
+        $(".remove-img").click(function(){
+          console.log(this);
+          $(this).parent(".img-thumbnail").remove();
+        })
+
+        });
+        reader.readAsDataURL(img);
+      }
+      */
+     
+      for( img of images){
+
+        console.log(img.lastModified);
+        let reader = new FileReader();
+        let figure = document.createElement("figure");
+        let a = document.createElement("a");
+        Irray.push(img)
+        let figcaption = document.createElement("figcaption");
+        figcaption.style.cssText = "font-size: 10px ; color: #777";
+        figcaption.innerHTML = img.name;
+        figure.style.cssText = "width: 100px;height: 100px; margin: 5px; padding:1px;position:relative; cursor:pointer;"
+        figure.setAttribute("id", img.lastModified);
+        figure.appendChild(figcaption);
+        console.log(Irray.indexOf(img));
+        reader.onload=()=>{
+          let imgTag = document.createElement("img");
+          imgTag.classList.add('img-thumbnail','img-fluid');
+          a.classList.add("stretched-link");
+          imgTag.setAttribute("src", reader.result);
+          figure.insertBefore(imgTag, figcaption);
+          // figure.appendChild(a);
+        }
+        preview.appendChild(figure);
+        reader.readAsDataURL(img);
+      
+        /**
+         * pass onclick="delete_image(images.indexOf(img))"
+         *  function delete_image(e) { images.splice(e,1)}
+         * 
+         * */  
+
+      }
+
+      $("figure").click(function(){
+        console.log(this);
+        $(this).remove();
+        // console.log($('#files').$(this));
+      })
+
+      
+
+    };
     $scope.dirlocation = datagrab.completeUrlLocation;
     $scope.currentPage = pager;
     $scope.pageSize = 5;
@@ -52,7 +130,7 @@ module.controller("categoryController", [
       $(".btn_" + id).toggle(500);
     };
 
-    $scope.loader_control = function(e){
+    $scope.loader_control = function (e) {
       $(e).hide(1000);
     };
 
@@ -77,7 +155,7 @@ module.controller("categoryController", [
           var msg = angular.fromJson(parsed);
           // $scope.loader_control('#category_loader');
           $("#category_loader").hide(500);
-         
+
           if (msg.status == "1") {
             $scope.all_cat_and_sub = msg.data;
 
@@ -104,7 +182,7 @@ module.controller("categoryController", [
         headers: { "gnice-authenticate": $scope.admin_token },
         processData: false,
         success: function (answer) {
-         console.log(answer);
+          console.log(answer);
           var response = JSON.stringify(answer);
           var parsed = JSON.parse(response);
           var msg = angular.fromJson(parsed);
@@ -112,7 +190,6 @@ module.controller("categoryController", [
 
           $("#add_cat_loader").hide(500);
           if (msg.status == "1") {
-          
             $(".result").html(msg.message);
             $(".result").show();
             $scope.get_all_cat_and_sub_cat();
@@ -124,10 +201,13 @@ module.controller("categoryController", [
         },
       });
     };
+    $scope.show_images = function () {
+      alert("got here");
+    };
     $scope.update_category = function (id) {
-      $(".edit_"+ id).show(500);
-      $(".icon_edit_"+ id).hide();
-      
+      $(".edit_" + id).show(500);
+      $(".icon_edit_" + id).hide();
+
       var formData = new FormData($("#updateCategory")[0]);
       $.ajax({
         url: $scope.dirlocation + "adminapi/update_category",
@@ -144,17 +224,16 @@ module.controller("categoryController", [
           var response = JSON.stringify(answer);
           var parsed = JSON.parse(response);
           var msg = angular.fromJson(parsed);
-         
-          $(".edit_"+ id).hide(500);
-          $(".icon_edit_"+ id).show();
-         
+
+          $(".edit_" + id).hide(500);
+          $(".icon_edit_" + id).show();
+
           if (msg.status == "1") {
-           
             $(".result-c").html(msg.message);
             $(".result-c").show(500);
             $scope.get_all_cat_and_sub_cat();
             $scope.$apply();
-           
+
             $("#updateCategory")[0].reset();
           } else {
             $(".result-c").html(msg.message);
@@ -170,11 +249,11 @@ module.controller("categoryController", [
       let id = subCat.sub_id;
       console.log(subCat.sub_id);
       console.log($index);
-      $(".edit_sub_"+ id).show(200);
-      $(".icon_sub_"+ id).hide(100);
-     
+      $(".edit_sub_" + id).show(200);
+      $(".icon_sub_" + id).hide(100);
+
       var formData = new FormData($("#update_sub_category_" + id)[0]);
-      $(".result-s"+ id).show();
+      $(".result-s" + id).show();
       $.ajax({
         url: $scope.dirlocation + "adminapi/update_sub_category",
         type: "POST",
@@ -189,51 +268,49 @@ module.controller("categoryController", [
           var response = JSON.stringify(answer);
           var parsed = JSON.parse(response);
           var msg = angular.fromJson(parsed);
-          $(".edit_sub_"+ id).hide(500);
-          $(".icon_sub_"+ id).show(100);
+          $(".edit_sub_" + id).hide(500);
+          $(".icon_sub_" + id).show(100);
           if (msg.status == "1") {
-         
-          let category = JSON.parse(localStorage.getItem('ngStorage-catInfo'));
-          
-          // category.filter((e)=>{
-          //   return e.sub_id == id
-          //   console.log(e);
-          // })
-          // for(let i = 0; i < category.subcategory.length; i++){
-            
-          //   if(element.sub_id == id){
+            let category = JSON.parse(
+              localStorage.getItem("ngStorage-catInfo")
+            );
 
-          //     category.subcategory.split(i, 1);
-          //     element.title = msg.title;
-  
-          //     if(msg.image != ''){
-          //       element.image = msg.image;
-          //     }
-  
-          //     console.log(category.subcategory);
-  
-          //     }
-          // }
+            // category.filter((e)=>{
+            //   return e.sub_id == id
+            //   console.log(e);
+            // })
+            // for(let i = 0; i < category.subcategory.length; i++){
 
-          category.subcategory.forEach(element => {
+            //   if(element.sub_id == id){
 
+            //     category.subcategory.split(i, 1);
+            //     element.title = msg.title;
 
-            if(element.sub_id == id){
+            //     if(msg.image != ''){
+            //       element.image = msg.image;
+            //     }
 
-             let index =  category.subcategory.indexOf(element);
+            //     console.log(category.subcategory);
 
-             element.title = msg.title;
-             
-             if(msg.image != ''){
-               element.image = msg.image;
+            //     }
+            // }
+
+            category.subcategory.forEach((element) => {
+              if (element.sub_id == id) {
+                let index = category.subcategory.indexOf(element);
+
+                element.title = msg.title;
+
+                if (msg.image != "") {
+                  element.image = msg.image;
+                }
+                category.subcategory.splice(index, 1, element);
               }
-            category.subcategory.splice(index, 1, element);
-            }
-          });
-          
-          // localStorage.setItem('ngStorage-catInfo',JSON.stringify(category));
-          $localStorage["catInfo"] = category;
-          $scope.$apply();
+            });
+
+            // localStorage.setItem('ngStorage-catInfo',JSON.stringify(category));
+            $localStorage["catInfo"] = category;
+            $scope.$apply();
             $(".result-s").html(msg.message);
             $(".result-s").addClass("alert alert-info");
             $(".result-s").show(500);
@@ -272,7 +349,7 @@ module.controller("categoryController", [
         processData: false,
         success: function (answer) {
           console.log(result);
-        
+
           var response = JSON.stringify(answer);
           var parsed = JSON.parse(response);
           var msg = angular.fromJson(parsed);
@@ -282,7 +359,7 @@ module.controller("categoryController", [
             $(".result").html(msg.message);
             $(".result").addClass("alert alert-info");
             $(".result").show(500);
-            
+
             setTimeout(() => {
               $(".result").hide("500");
               $(".result").removeClass("alert alert-info");
@@ -305,10 +382,9 @@ module.controller("categoryController", [
       });
     };
 
-    
     $scope.enable_disable_sub = function (code, sub) {
-      $('.sub_loader_'+ sub.sub_id).show();
-      $('.icon_'+ sub.sub_id).hide();
+      $(".sub_loader_" + sub.sub_id).show();
+      $(".icon_" + sub.sub_id).hide();
       var formData = new FormData();
 
       formData.append("status", code);
@@ -328,10 +404,9 @@ module.controller("categoryController", [
           var response = JSON.stringify(result);
           var parsed = JSON.parse(response);
           var msg = angular.fromJson(parsed);
-          $('.sub_loader_'+ sub.sub_id).hide(500);
-          $('.icon_'+ sub.sub_id).show();
+          $(".sub_loader_" + sub.sub_id).hide(500);
+          $(".icon_" + sub.sub_id).show();
           if (msg.status == "1") {
-            
             $(".result").html(msg.message);
             $(".result").addClass("alert alert-info");
             $(".result").show(500);
@@ -342,7 +417,6 @@ module.controller("categoryController", [
               $(".result").removeClass("alert alert-info");
             }, 3000);
           } else {
-    
             $(".result").html(msg.message);
             $(".result").addClass("alert alert-info");
             $(".result").show(500);
@@ -356,11 +430,10 @@ module.controller("categoryController", [
       });
     };
     $scope.enable_or_disable_cat = function (code, cat) {
-      $(".cat_loader_"+ cat.id).show(500);
-      $(".icon_toggle_"+ cat.id).hide(100);
-      
+      $(".cat_loader_" + cat.id).show(500);
+      $(".icon_toggle_" + cat.id).hide(100);
+
       var formData = new FormData();
-     
 
       formData.append("status", code);
       formData.append("category_id", cat.id);
@@ -378,11 +451,10 @@ module.controller("categoryController", [
           var response = JSON.stringify(result);
           var parsed = JSON.parse(response);
           var msg = angular.fromJson(parsed);
-         
-          $(".cat_loader_"+ cat.id).hide(500);
-          $(".icon_toggle_"+ cat.id).show(100);
+
+          $(".cat_loader_" + cat.id).hide(500);
+          $(".icon_toggle_" + cat.id).show(100);
           if (msg.status == "1") {
-            
             $(".result").html(msg.message);
             $(".result").addClass("alert alert-info");
             $(".result").show(500);
@@ -411,7 +483,8 @@ module.controller("categoryController", [
     $scope.localStorage_get = function (key) {
       $scope[key] = $localStorage[key];
     };
-
+    // $(".files[]").change(function(){
+    //   alert("The text has been changed.");
+    // });
   },
-  
 ]);
